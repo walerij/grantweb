fb3bee579e62ff8c06622181e3584333134672db
Administrator <becwal@yandex.ru>
2018-02-03 17:18:30 +0300

    Стиль топа и главного меню

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,20 +26,50 @@ AppAsset::register($this);
 <?php $this->beginBody() ?>
 
 <div class="wrap">
+
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
+        /*'brandLabel' => 'Гранд',
+        'brandUrl' => Yii::$app->homeUrl,*/
         'options' => [
-            'class' => 'navbar-default navbar-fixed-top navTopmenu',
+            'class' => 'navbar-default navbar-fixed-top navTopSearch',
+        ],
+    ]);  echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container" style="height: 220px;">
+
+    </div>
+
+    <?php
+    NavBar::begin([
+        /**'brandLabel' => 'My Company',
+        'brandUrl' => Yii::$app->homeUrl,*/
+        'options' => [
+            'class' => 'navbar-default navTopmenu',
         ],
     ]);
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav navbar-leat'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Главная', 'url' => ['/site/index']],
+            ['label' => 'О нас', 'url' => ['/site/about']],
+            ['label' => 'Новости', 'url' => ['/site/about1']],
+            ['label' => 'Дома', 'url' => ['/site/about2']],
+            ['label' => 'Другие обслуживаемые мкд', 'url' => ['/site/about3']],
+            ['label' => 'Отчетность', 'url' => ['/site/about4']],
+            ['label' => 'Информация', 'url' => ['/site/about5']],
+            ['label' => 'Отзывы', 'url' => ['/site/about6']],
+
+
             ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Кабинет', 'url' => ['/site/about8']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
